8afa62104e3b85aa55440e79a2c9211960225ba5
tl1l1l1s <s2oo1og24@gmail.com>
2026-01-18 21:49:46 +0900

    Initial commit

---
+++ src/test/java/com/sipomeokjo/commitme/CommitMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sipomeokjo.commitme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommitMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
