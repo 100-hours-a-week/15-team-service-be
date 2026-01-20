ba782bd3078800507689c8add64a86a9e91d1170
tl1l1l1s <s2oo1og24@gmail.com>
2026-01-20 11:51:11 +0900

    feat: add success API response

--- src/main/java/com/sipomeokjo/commitme/api/response/ApiResponse.java
+++ src/main/java/com/sipomeokjo/commitme/api/response/ApiResponse.java
@@ -1,7 +1,36 @@
 package com.sipomeokjo.commitme.api.response;
 
-public record ApiResponse<T> (
-		String code,
-		String message,
-		T data
-) {}
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.springframework.http.ResponseEntity;
+
+@JsonPropertyOrder({ "code", "message", "data" })
+public record ApiResponse<T>(
+        String code,
+        String message,
+        T data
+) {
+
+    public static <T> ResponseEntity<ApiResponse<T>> onSuccess(SuccessCode successCode, T data) {
+        ApiResponse<T> body = new ApiResponse<>(successCode.getCode(), successCode.getMessage(), data);
+        return new ResponseEntity<>(body, successCode.getHttpStatus());
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> onSuccess(SuccessCode successCode) {
+        ApiResponse<T> body = new ApiResponse<>(successCode.getCode(), successCode.getMessage(), null);
+        return new ResponseEntity<>(body, successCode.getHttpStatus());
+    }
+
+    public static ResponseEntity<Void> noContent(SuccessCode successCode) {
+        return new ResponseEntity<>(successCode.getHttpStatus());
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> onFailure(ErrorCode errorCode, T data) {
+        ApiResponse<T> body = new ApiResponse<>(errorCode.getCode(), errorCode.getMessage(), data);
+        return new ResponseEntity<>(body, errorCode.getHttpStatus());
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> onFailure(ErrorCode errorCode) {
+        ApiResponse<T> body = new ApiResponse<>(errorCode.getCode(), errorCode.getMessage(), null);
+        return new ResponseEntity<>(body, errorCode.getHttpStatus());
+    }
+}
