0a60913286b5b802e40f7e2a9c81e6b840067abc
minzer01 <minzero@ewhain.net>
2026-01-30 03:10:34 +0900

    제목명 수정 + 생성 성공한 이력서만 조회

--- src/main/java/com/sipomeokjo/commitme/domain/resume/repository/ResumeRepository.java
+++ src/main/java/com/sipomeokjo/commitme/domain/resume/repository/ResumeRepository.java
@@ -4,6 +4,8 @@ import com.sipomeokjo.commitme.domain.resume.entity.Resume;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -12,4 +14,12 @@ public interface ResumeRepository extends JpaRepository<Resume, Long> {
     Page<Resume> findByUser_Id(Long userId, Pageable pageable);
 
     Optional<Resume> findByIdAndUser_Id(Long resumeId, Long userId);
+
+    /**
+     * SUCCEEDED 상태의 버전이 있는 이력서만 조회
+     */
+    @Query("SELECT DISTINCT r FROM Resume r " +
+           "JOIN ResumeVersion rv ON rv.resume = r " +
+           "WHERE r.user.id = :userId AND rv.status = 'SUCCEEDED'")
+    Page<Resume> findSucceededByUserId(@Param("userId") Long userId, Pageable pageable);
 }
