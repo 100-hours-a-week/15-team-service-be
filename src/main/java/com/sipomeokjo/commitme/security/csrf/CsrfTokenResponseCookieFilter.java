eeab896419505f5019f906ae8d3680a6288bfc34
tl1l1l1s <s2oo1og24@gmail.com>
2026-02-01 12:19:00 +0900

    fix: csrf error

--- src/main/java/com/sipomeokjo/commitme/security/csrf/CsrfTokenResponseCookieFilter.java
+++ src/main/java/com/sipomeokjo/commitme/security/csrf/CsrfTokenResponseCookieFilter.java
@@ -5,10 +5,17 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@RequiredArgsConstructor
 public class CsrfTokenResponseCookieFilter extends OncePerRequestFilter {
+
+    private final CsrfTokenRepository csrfTokenRepository;
 
     @Override
     protected void doFilterInternal(
@@ -18,7 +25,14 @@ public class CsrfTokenResponseCookieFilter extends OncePerRequestFilter {
         if (csrfToken == null) {
             csrfToken = (CsrfToken) request.getAttribute("_csrf");
         }
-        if (csrfToken != null) {
+		
+        if (HttpMethod.GET.matches(request.getMethod())
+                || HttpMethod.HEAD.matches(request.getMethod())) {
+            if (csrfToken == null) {
+                csrfToken = csrfTokenRepository.generateToken(request);
+            }
+            csrfTokenRepository.saveToken(csrfToken, request, response);
+        } else if (csrfToken != null) {
             csrfToken.getToken();
         }
         filterChain.doFilter(request, response);
