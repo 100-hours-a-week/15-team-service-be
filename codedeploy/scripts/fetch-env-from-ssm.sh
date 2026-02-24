55a5b8c7b6366e0f9500358ed87caaa5a45b6d83
Day HONG <tot032874@gmail.com>
2026-02-24 13:04:11 +0900

    ci/be-v2-2 : flyway version setting error fixed

--- codedeploy/scripts/fetch-env-from-ssm.sh
+++ codedeploy/scripts/fetch-env-from-ssm.sh
@@ -79,7 +79,7 @@ spring:
     enabled: true
     locations: classpath:db/migration
     baseline-on-migrate: true
-    baseline-version: "20260223.0"
+    baseline-version: "20260204.0"
     
 logging:
   level:
