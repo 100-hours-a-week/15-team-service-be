fc4a74577cc1adf60433ce9ded568ccee09d4b6a
Day HONG <tot032874@gmail.com>
2026-03-10 10:24:22 +0900

    alloy volume set & add instance_id label to alloy

--- codedeploy/scripts/start.sh
+++ codedeploy/scripts/start.sh
@@ -9,4 +9,12 @@ export SPRING_CONFIG_FILE="${APP_DIR}/application-prod.yml"
 export SPRING_CONFIG_BASENAME="application-prod.yml"
 export ALLOY_CONFIG_FILE="${APP_DIR}/alloy/alloy.config"
 
+TOKEN=$(curl -sS -X PUT "http://169.254.169.254/latest/api/token" \
+  -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+INSTANCE_ID=$(curl -sS -H "X-aws-ec2-metadata-token: $TOKEN" \
+  http://169.254.169.254/latest/meta-data/instance-id)
+
+export ALLOY_INSTANCE_ID="$INSTANCE_ID"
+
 docker compose -f "${APP_DIR}/docker-compose.yml" up -d --remove-orphans
